5b15b48fde9b94b2fa5ad8cd6511bb814cf22925
Дарын <241245@astanait.edu.kz>
2025-01-20 13:00:05 +0300

    created constructor

--- src/main/java/models/Course.java
+++ src/main/java/models/Course.java
@@ -4,4 +4,14 @@ public class Course {
     private int course_id;
     private String name;
     private Double attendance;
+
+    public Course(){
+
+    }
+
+    public Course(int course_id,String name,Double attendance){
+        this.course_id = course_id;
+        this.name = name;
+        this.attendance = attendance;
+    }
 }
