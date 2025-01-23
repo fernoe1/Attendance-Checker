229f4a9957804ef76ddb067aa2fb97558c20f40a
Дарын <241245@astanait.edu.kz>
2025-01-23 19:48:38 +0300

    created 1 parametrized and one non-parametrized.

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -5,4 +5,12 @@ public class Person {
     private String surname;
     private String user_id;
     private int barcode;
+
+    public Person(){
+
+    }
+
+    public Person(String name,String surname,String user_id,int barcode){
+        //setter i will do it in getter and setters
+    }
 }
