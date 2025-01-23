6e8a654433380c121928edcab0e09e32265c27df
Дарын <241245@astanait.edu.kz>
2025-01-23 20:14:01 +0300

    created new fields for Person class

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -4,11 +4,4 @@ public class Person {
     private String name;
     private String surname;
 
-    public Person(){
-
-    }
-
-    public Person(String name,String surname,String user_id,int barcode){
-        //setter i will do it in getter and setters
-    }
 }
