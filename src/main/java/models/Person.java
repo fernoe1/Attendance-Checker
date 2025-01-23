b6e03ef7b9e1dbe24e7d2d27a00bbf93fef9f71c
Дарын <241245@astanait.edu.kz>
2025-01-23 20:03:15 +0300

    created 1 parametrized and one non-parametrized.

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,31 +1,6 @@
 package models;
 
 public class Person {
-    private String name ;
-    private String surname ;
-
-    public Person(){
-
-    }
-
-    public Person(String name,String surname){
-        setName(name);
-        setSurname(surname);
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+    private String name;
+    private String surname;
 }
